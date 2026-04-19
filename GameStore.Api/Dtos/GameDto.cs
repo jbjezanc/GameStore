056fc27fd4dbdcb59e8bb08ff67140b4236f98ba
Josip Bjezancevic <josip.bjezancevic@gmail.com>
2026-04-19 13:58:29 +0200

    Initial setup and routes

---
+++ GameStore.Api/Dtos/GameDto.cs
@@ -0,0 +1,12 @@
+namespace GameStore.Api.Dtos;
+
+// A DTO is a contract between the client and server since it represents
+// a shared agrement about how data will be trasferred and used.
+
+public record GameDto(
+    int Id,
+    string Name,
+    string Genre,
+    decimal Price,
+    DateOnly ReleaseDate
+);
