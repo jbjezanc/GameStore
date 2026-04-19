335787ab2b170c0b379776c06aa5b6678d5df456
Josip Bjezancevic <josip.bjezancevic@gmail.com>
2026-04-19 22:32:20 +0200

    Define Game and Genre models

--- GameStore.Api/obj/Debug/net10.0/GameStore.Api.AssemblyInfo.cs
+++ GameStore.Api/obj/Debug/net10.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd924790dee0a4bed0d27570e810eca5a7b447c9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4fe4c6734ff428bf3c5982756575d6c72aef4902")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
